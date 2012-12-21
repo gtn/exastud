a59fac6c2db438322aff89a3873b73de40c0165a
Florian Jungwirth <jungwirth.florian@gmail.com>
2012-12-21 14:16:19 +0100

    version update

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-    $plugin->version = 2012082300;  // YYYYMMDDHH (year, month, day, 24-hr time)
+    $plugin->version = 2012122100;  // YYYYMMDDHH (year, month, day, 24-hr time)
     $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 ?>
