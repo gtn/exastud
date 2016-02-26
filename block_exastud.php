9e4118d916b60b6fcc784b04db456af7f831b2a4
dprieler <dprieler@gmail.com>
2016-02-26 10:12:03 +0100

    gtn copyright

--- block_exastud.php
+++ block_exastud.php
@@ -1,34 +1,22 @@
 <?php
-
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Exabis Student Review
 //
-// Moodle is free software: you can redistribute it and/or modify
+// (c) 2016 GTN - Global Training Network GmbH <office@gtn-solutions.com>
+//
+// Exabis Student Review is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// This script is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// You can find the GNU General Public License at <http://www.gnu.org/licenses/>.
+//
+// This copyright notice MUST APPEAR in all copies of the script!
 
-// All rights reserved
-/**
- * @package moodlecore
- * @subpackage blocks
- * @copyright 2013 gtn gmbh
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- 
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
-*/
 require_once __DIR__.'/lib/lib.php';
 require_once __DIR__.'/../moodleblock.class.php';
 
