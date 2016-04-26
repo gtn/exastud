01767b19723d60d8dcbdfb4201404955659de361
dprieler <dprieler@gmail.com>
2016-04-26 12:48:50 +0200

    Update version.php

--- version.php
+++ version.php
@@ -20,7 +20,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exastud';
-$plugin->release  = "3.0.1";
+$plugin->release  = "3.0.1.experimental";
 $plugin->version   = 2016041400;
 $plugin->requires  = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
