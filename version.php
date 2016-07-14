7731008e0f610639cee530e2fbf36c50c25c8f7c
dprieler <dprieler@gmail.com>
2016-07-14 09:51:37 +0200

    version

--- version.php
+++ version.php
@@ -20,7 +20,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exastud';
-$plugin->release  = "3.0.1.experimental";
+$plugin->release  = "3.0.2.master";
 $plugin->version   = 2016042902;
 $plugin->requires  = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
