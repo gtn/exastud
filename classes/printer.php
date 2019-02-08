f6cdbbbdf417f69894a24de2e409db03731ad30c
Sergey Zavarzin <razawa@ukr.net>
2019-02-08 10:58:24 +0200

    reports: cleaning for xml

--- classes/printer.php
+++ classes/printer.php
@@ -2669,7 +2669,55 @@ class TemplateProcessor extends \PhpOffice\PhpWord\TemplateProcessor {
 		}
 	}
 
+    function sanitizeXML($string)
+    {
+        if (!empty($string))
+        {
+            // remove EOT+NOREP+EOX|EOT+<char> sequence (FatturaPA)
+            $string = preg_replace('/(\x{0004}(?:\x{201A}|\x{FFFD})(?:\x{0003}|\x{0004}).)/u', '', $string);
+            $regex = '/(
+            [\xC0-\xC1] # Invalid UTF-8 Bytes
+            | [\xF5-\xFF] # Invalid UTF-8 Bytes
+            | \xE0[\x80-\x9F] # Overlong encoding of prior code point
+            | \xF0[\x80-\x8F] # Overlong encoding of prior code point
+            | [\xC2-\xDF](?![\x80-\xBF]) # Invalid UTF-8 Sequence Start
+            | [\xE0-\xEF](?![\x80-\xBF]{2}) # Invalid UTF-8 Sequence Start
+            | [\xF0-\xF4](?![\x80-\xBF]{3}) # Invalid UTF-8 Sequence Start
+            | (?<=[\x0-\x7F\xF5-\xFF])[\x80-\xBF] # Invalid UTF-8 Sequence Middle
+            | (?<![\xC2-\xDF]|[\xE0-\xEF]|[\xE0-\xEF][\x80-\xBF]|[\xF0-\xF4]|[\xF0-\xF4][\x80-\xBF]|[\xF0-\xF4][\x80-\xBF]{2})[\x80-\xBF] # Overlong Sequence
+            | (?<=[\xE0-\xEF])[\x80-\xBF](?![\x80-\xBF]) # Short 3 byte sequence
+            | (?<=[\xF0-\xF4])[\x80-\xBF](?![\x80-\xBF]{2}) # Short 4 byte sequence
+            | (?<=[\xF0-\xF4][\x80-\xBF])[\x80-\xBF](?![\x80-\xBF]) # Short 4 byte sequence (2)
+        )/x';
+            $string = preg_replace($regex, '', $string);
+
+            $result = "";
+            $length = strlen($string);
+            for ($i=0; $i < $length; $i++)
+            {
+                $current = ord($string{$i});
+                if (($current == 0x9) ||
+                        ($current == 0xA) ||
+                        ($current == 0xD) ||
+                        (($current >= 0x20) && ($current <= 0xD7FF)) ||
+                        (($current >= 0xE000) && ($current <= 0xFFFD)) ||
+                        (($current >= 0x10000) && ($current <= 0x10FFFF)))
+                {
+                    $result .= chr($current);
+                }
+                else
+                {
+                    // $ret;    // use this to strip invalid character(s)
+                    // $ret .= " ";    // use this to replace them with spaces
+                }
+            }
+            $string = $result;
+        }
+        return $string;
+    }
+
 	function setValue($search, $replace, $limit = self::MAXIMUM_REPLACEMENTS_DEFAULT) {
+        $replace = $this->sanitizeXML($replace);
 		$replace = $this->escape($replace);
 		// if the marker ${marker} is in the some element (form, textblock,...) he is inserting in something like w:val="${marker}"
         // and we does not need to replace linebreaks.
