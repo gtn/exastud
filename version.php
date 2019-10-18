f13aa9fac27bcda41a03d14a460b4ed0e305f7ab
Dietmar Angerer <uab.katzinger@gmail.com>
2019-10-18 08:20:37 +0200

    increase release number

--- version.php
+++ version.php
@@ -21,7 +21,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exastud';
 
-$plugin->release  = "4.6.3";
+$plugin->release  = "4.6.4";
 $plugin->version   = 2019100700;
 $plugin->requires  = 2015051100;
 $plugin->maturity = MATURITY_ALPHA;
