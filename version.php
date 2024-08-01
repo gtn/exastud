aa268f8575b94aaea5f487d54eff4ac445513815
Dietmar Angerer <uab.katzinger@gmail.com>
2024-08-01 11:38:38 +0200

    bump version and changelog

--- version.php
+++ version.php
@@ -22,6 +22,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_exastud';
 
 $plugin->release  = "4.6.6";
-$plugin->version   = 2023091601;
+$plugin->version   = 2024080100;
 $plugin->requires  = 2021051700; // moodle 3.11
 $plugin->maturity = MATURITY_STABLE;
