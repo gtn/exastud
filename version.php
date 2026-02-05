50861e430a221052d192a7a4cb2dc4f63f9b9081
prowebat <1819129+prowebat@users.noreply.github.com>
2026-02-05 23:34:24 +0800

    bump version

--- version.php
+++ version.php
@@ -21,7 +21,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exastud';
 
-$plugin->release = "5.1";
+$plugin->release = '5.1';
 $plugin->version = 2026020500;
 $plugin->requires = 2021051700; // moodle 3.11
 $plugin->maturity = MATURITY_STABLE;
